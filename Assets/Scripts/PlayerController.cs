bead8a0776f0b019281c63532020e2118b125b66
39xdgy <wangjiashu3@126.com>
2018-03-20 19:27:12 -0500

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text win;
+
+    private Rigidbody rb;
+    private int count;
+    
+
+	void Start() {
+		rb = GetComponent<Rigidbody> ();
+        count = 0;
+        SetCOuntText();
+        win.text = "";
+    }
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+    void OnTriggerEnter(Collider other){
+        //       Destroy(other.gameObject);
+        if (other.gameObject.CompareTag("Pickup")){
+            other.gameObject.SetActive(false);
+            count++;
+            SetCOuntText();
+        }
+
+    }
+
+    void SetCOuntText(){
+        countText.text = "Count: " + count.ToString();
+        if (count == 13)
+        {
+            win.text = "You Win";
+        }
+    }
+
+
+}
