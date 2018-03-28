34d1db3763fe9652a43d7f8f6fa7ee9586234b74
STOzaki <SOzaki19@cornellcollege.edu>
2018-03-27 22:40:07 -0500

    Finished gravity
    I finished adding a class that will turn off and on the gravity of the ball

---
+++ Assets/Scripts/GripGrav.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripGrav : MonoBehaviour {
+    private bool grav = true;
+    private Rigidbody gravity;
+    private SteamVR_TrackedObject controller;
+
+
+	// Use this for initialization
+	void Awake () {
+		this.controller = GetComponent<SteamVR_TrackedObject>();
+        this.gravity = GameObject.Find("Ball").GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var device = SteamVR_Controller.Input((int)controller.index);
+
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            if(grav)
+            {
+                gravity.useGravity = false;
+                grav = false;
+            }
+            else
+            {
+                gravity.useGravity = true;
+                grav = true;
+            }
+        }
+	}
+}
